6a57ff10dec31bf3b23f62e9b70077a1bacfea41
Makinohara <aburider@proton.me>
2026-04-14 18:31:55 +0000

    earth: Update blobs to V816.0.16.UCVMIXM
    Signed-off-by: Makinohara <aburider@proton.me>

--- Android.mk
+++ Android.mk
@@ -6,14 +6,14 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),earth)
 
-$(call add-radio-file-sha1-checked,radio/gz.img,e3b488b2d0fc0d355562ae8670ccaef28e4b089d)
-$(call add-radio-file-sha1-checked,radio/lk.img,8417693289a641030b7803db818d3fab23b7b516)
-$(call add-radio-file-sha1-checked,radio/logo.img,fcb8ef806923912e68a784432e123425d6a1a976)
-$(call add-radio-file-sha1-checked,radio/md1img.img,51b91f485f1058de10a3795f0f9c3c7018d4d707)
-$(call add-radio-file-sha1-checked,radio/preloader_raw.img,0ab2ac164de3b98da467acde0dbda752a0ca1fbf)
-$(call add-radio-file-sha1-checked,radio/scp.img,90bc6a471fc2c00f95ae09cba63140a6f74ceb3f)
-$(call add-radio-file-sha1-checked,radio/spmfw.img,db9a5346bcd53c76f652829a523558bb270377ac)
-$(call add-radio-file-sha1-checked,radio/sspm.img,b52a92a86716a3f69fc487aa52529e9b26b35fac)
-$(call add-radio-file-sha1-checked,radio/tee.img,d5a791e96d708c794d5d914fcbe6bb8ce8741571)
+$(call add-radio-file-sha1-checked,radio/gz.img,d295d48b7a8418146c40609ac6079fe5c2213450)
+$(call add-radio-file-sha1-checked,radio/lk.img,f257e37997f82c97cc85b245d25cd572bf3f21e7)
+$(call add-radio-file-sha1-checked,radio/logo.img,19d23143572b78bd95b8e8e8cd5ee990775947f5)
+$(call add-radio-file-sha1-checked,radio/md1img.img,620cebbd411b21e78e2cec820b16e8e455e7b599)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,fc6fcdd366148679553ade0f81c825ceb73bbbbb)
+$(call add-radio-file-sha1-checked,radio/scp.img,88826f1c4c08cda42916e08ca6978df8f96238d8)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,b5730aa21d4fe8b2e436a9453d829ac0c03a2e65)
+$(call add-radio-file-sha1-checked,radio/sspm.img,48e649b4854abf1415cf607c7d5368b2a0243198)
+$(call add-radio-file-sha1-checked,radio/tee.img,9b13cd70933a0a2162f18dc4800747a90e880203)
 
 endif
